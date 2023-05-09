e75a3cb5a617649bf6a7c0818fad67f418cf5b09
JosePu1201 <joseluis-pusam@cunoc.edu.gt>
2023-05-09 01:11:01 -0600

    Validacion de referencia y tabla repetida

--- Proyecto_2_EDD/src/main/java/com/mycompany/proyecto_2_edd/ListaTablas/ListaTabla.java
+++ Proyecto_2_EDD/src/main/java/com/mycompany/proyecto_2_edd/ListaTablas/ListaTabla.java
@@ -9,33 +9,76 @@ package com.mycompany.proyecto_2_edd.ListaTablas;
  * @author jose
  */
 public class ListaTabla {
+
     private NodoTabla primero;
     private NodoTabla ultimo;
-    
+
     public ListaTabla() {
-    
+
     }
-    
-    public void agregarFinal(NodoTabla nuevo){
-        if(primero == null){
+
+    public void agregarFinal(NodoTabla nuevo) {
+
+        if (repetido(nuevo)) {//valida repitencia de nombre
+            if (nuevo.getTabla().getRelacion() == null) {
+                agregar(nuevo);
+            } else {//valida si existe referencia 
+                if (validarRef(nuevo)) {
+                    agregar(nuevo);
+                } else {
+                    System.out.println("Fallo la referencia");
+                }
+            }
+
+        } else {
+            System.out.println("El nombre se repito");
+        }
+    }
+
+    public void agregar(NodoTabla nuevo) {
+        if (primero == null) {
             this.primero = nuevo;
             this.ultimo = nuevo;
-        }
-        else{
+        } else {
             ultimo.setSiguiente(nuevo);
             ultimo.getSiguiente().setAnterior(nuevo);
-            
             ultimo = ultimo.getSiguiente();
         }
-        
     }
-    
-    public void listar(){
+
+    public void listar() {
         NodoTabla aux = primero;
-        
-        while (aux != null) {            
+
+        while (aux != null) {
             System.out.println(aux.toString());
             aux = aux.getSiguiente();
         }
     }
+
+    public boolean validarRef(NodoTabla nuevo) {
+        boolean valido = false;
+        NodoTabla aux = primero;
+        while (aux != null) {
+            if (aux.getTabla().getNombre().equals(nuevo.getTabla().getRelacion())) {
+                valido = true;
+                break;
+            }
+            aux = aux.getSiguiente();
+        }
+        return valido;
+    }
+
+    public boolean repetido(NodoTabla nuevo) {
+        boolean valido = true;
+        NodoTabla aux = primero;
+        while (aux != null) {
+            if (aux.getTabla().getNombre().equals(nuevo.getTabla().getNombre())) {
+                valido = false;
+                break;
+            }
+            aux = aux.getSiguiente();
+        }
+        return valido;
+    }
+
 }
